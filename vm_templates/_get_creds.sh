2de6cf6b25d7b860a5dc79c57ea5d3769a481476
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2023-12-11 20:22:08 -0700

    try and fail xo cmd

--- vm_templates/_get_creds.sh
+++ vm_templates/_get_creds.sh
@@ -1,18 +1,13 @@
 #!/bin/env bash
 # Change the remote host and sr_name lines if you want to deploy to a different host
-if [ ! -f creds.json ]; then
+if [ ! -f creds.sh ]; then
 	echo "Credentials file doesn't already exist, loading from Bitwarden."
 	echo "Logging into bitwarden"
-	# bw login
+	bw login
 	echo "Getting the xen orchestra creds"
-	touch creds.json
-	echo "{" > creds.json
-	echo "\"remote_host\": \"d-hpp-1.ipreston.net\"," >> creds.json
-	USER=root
+	touch creds.sh
+	USER=ipreston
 	PASS=$(bw get password xo.ipreston.net)
-	echo "\"remote_username\": \"$USER\"," >> creds.json
-	echo "\"remote_password\": \"$PASS\"," >> creds.json
-	echo "\"sr_iso_name\": \"Synology ISOs\"," >> creds.json
-	echo "\"sr_name\": \"templates-dhpp1\"" >> creds.json
-	echo "}" >> creds.json
+	echo "XOUSER='$USER'" >> creds.sh
+	echo "XOPASS='$PASS'" >> creds.sh
 fi
