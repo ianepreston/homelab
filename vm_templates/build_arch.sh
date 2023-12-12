78f1e8f8947d574d388fe9fc3e4a003a2ca2189c
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2023-12-12 14:29:33 -0700

    revert back from xo-cli options

--- vm_templates/build_arch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/env bash
-bash _get_creds.sh
-packer init arch/arch.pkr.hcl
-packer build -var-file="creds.json" arch/arch.pkr.hcl
--- vm_templates/build_ubuntu.sh
+++ vm_templates/build_ubuntu.sh
@@ -1,12 +1,4 @@
 #!/bin/env bash
 bash _get_creds.sh
-source creds.sh
-if [ ! -f /tmp/focal-server-cloudimg-amd64.img ]; then
-	wget -O /tmp/focal-server-cloudimg-amd64.img https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
-fi
-
-if [ ! -f /tmp/focal-server-cloudimg-amd64.vhd ]; then
-	qemu-img convert -f qcow2 /tmp/focal-server-cloudimg-amd64.img -O vpc /tmp/focal-server-cloudimg-amd64.vhd
-fi
-
-xo-cli --register --allowUnauthorized http://xo.ipreston.net $XOUSER $XOPASS
+packer init ubuntu/ubuntu-2004.pkr.hcl
+packer build -var-file="creds.json" ubuntu/ubuntu-2004.pkr.hcl
