5dc3d6175414d6e564e71f915ce8996d19224ef9
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-01-29 13:25:13 -0700

    verbose output

--- ansible/roles/dockerserver/ytdl/files/run_cron.sh
+++ ansible/roles/dockerserver/ytdl/files/run_cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'Cron started, running ytdl-sub...'
-ytdl-sub --config=/config/config.yaml sub /config/subscriptions.yaml
+ytdl-sub --config=/config/config.yaml sub /config/subscriptions.yaml -l verbose
 echo 'Finished running ytdl-sub'
