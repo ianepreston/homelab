be58fc8b23fcda92ff04c00a23e685ec59adeabc
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-12-21 18:44:11 -0700

    move to base folder
    kubernetes should be separate from talos config

---
+++ talos/flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Setup env for working in my kubernetes clusters";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              talosctl
+              kubectl
+              kubernetes-helm
+              clusterctl
+              argocd
+              cilium-cli
+              #bitwarden-secrets
+              bws
+              bitwarden-cli
+            ];
+          };
+        }
+      );
+    };
+}
