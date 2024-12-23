642386056dd8806294515defa974e69e14176896
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-12-23 12:41:24 -0700

    switch to cilium for CNI

--- talos/dev/genconfig.sh
+++ talos/dev/genconfig.sh
@@ -3,6 +3,7 @@ talosctl gen config dk8s https://192.168.40.13:6443 \
     --with-secrets secrets.yaml \
     --config-patch @patches/allow-controlplane-workloads.yaml \
     --config-patch @patches/dhcp.yaml \
+    --config-patch @patches/cilium.yaml \
     --config-patch @patches/install-disk.yaml \
     --config-patch @patches/interface-names.yaml \
     --config-patch @patches/kubelet-certificates.yaml \
