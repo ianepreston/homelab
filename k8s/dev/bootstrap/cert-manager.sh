db409ee852494ad8543df45eeacced52e1433bf5
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-12-25 13:07:20 -0700

    cleaner helm install
    Don't pipe to file, just pipe to the command

--- k8s/dev/bootstrap/cert-manager.sh
+++ k8s/dev/bootstrap/cert-manager.sh
@@ -5,14 +5,13 @@ echo "This has to be done before Bitwarden Secrets or Argo"
 helm repo add jetstack https://charts.jetstack.io --force-update
 # Figure out what version of cert-manager to install
 export certManagerVersion=$(cat ../services/certmanager/chart/Chart.yaml | yq eval '.dependencies[0].version')
-# Grab the values from the app chart for certmanager
-cat ../services/certmanager/chart/values.yaml | yq '.["cert-manager"]' > certmanager-vals.yaml
-# Install cert-manager
-helm install cert-manager \
+# Grab the values from the app chart for certmanager and install with helm
+cat ../services/certmanager/chart/values.yaml | yq '.["cert-manager"]' | \
+  helm install cert-manager \
   --create-namespace \
   --namespace cert-manager \
   --version $certManagerVersion \
   jetstack/cert-manager \
-  --values certmanager-vals.yaml
+  --values -
 # Install the bitwarden self signed tls cert
 kubectl apply -f ../services/certmanager/chart/templates/bitwarden-self-signed-cert.yaml
