1d598ecaf8a64e242744290d25191c8da5d0fdda
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-12-31 08:25:16 -0700

    remove the bootstrap secret adding
    Not used anymore

--- k8s/dev/bootstrap/external-secrets.sh
+++ k8s/dev/bootstrap/external-secrets.sh
@@ -22,9 +22,6 @@ export PROJECT_ID=$(bws project list | jq -r '.[0].id')
 export ORGANIZATION_ID=$(bws project list | jq -r '.[0].organizationId')
 echo $PROJECT_ID
 echo $ORGANIZATION_ID
-# Add them to bws so I can use them in external secrets - real bootstrappy/recursive I know
-bws secret create projectid $PROJECT_ID $PROJECT_ID
-bws secret create organizationid $ORGANIZATION_ID $PROJECT_ID
 # Parse out the argo vault plugin substitution from the yaml so it can be applied
 cat external-secret-store.yaml |\
   sed -e "s|<path:bitwardenids#organizationid>|${ORGANIZATION_ID}|g" \
