51703e77eabb6946101610d631b381dbbe69d736
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2025-04-28 15:25:38 -0600

    migrate to v5
    God that sucked

--- terraform/k8s/.terraform.lock.hcl
+++ terraform/k8s/.terraform.lock.hcl
@@ -3,16 +3,9 @@
 
 provider "registry.terraform.io/cloudflare/cloudflare" {
   version     = "5.3.0"
-  constraints = ">= 4.0.0"
+  constraints = "5.3.0"
   hashes = [
-    "h1:0+Cd0m7VaJVvrGmrf1ZJwHs/YgdIcks1PVMYadrElXc=",
-    "h1:ASK1oT+wl2MDATPNL3PVr76A1QEEqwJPI6n8TGHHcF4=",
-    "h1:AgT/tF7e/kFzVpsBEdAp1yxR2lMCHARm094RG57TVLg=",
-    "h1:CDIOBCvQ+1uhDGRSG6OM3kB0ZorvRtBw1a4etN+3IPc=",
-    "h1:FAQS7p1dH8ux8oYpXv0/ywLPU01ngB8f6fjz+3eVcOc=",
-    "h1:JInoYqg0b/qM/wgo5fu6eb+jqw9t+tnW3O5WPnOMdsc=",
     "h1:TEXKKaRkcSnbidtu+HemZWPznJCetYNTpAvZ5vUbIZ0=",
-    "h1:YCX5pUsJIfjUsO2M+P1FGKTJjVEd9gTi7pD3jTXklDs=",
     "zh:2f7b526dc7bdea594214b7428bf02a80643a0b53258d4981c747dae6dcfe2b57",
     "zh:b96462bf725e12d7e497f2166948aa7cce864a399b8625a791137eef63eb6b24",
     "zh:bd77b9b3ab4470e410a1c055cff279db44c8b7764721d9cbb8f1c0297ababa67",
--- terraform/k8s/cert-manager.tf
+++ terraform/k8s/cert-manager.tf
@@ -1,17 +1,35 @@
 # Zone permissions
-data "cloudflare_api_token_permission_groups" "all" {}
+data "cloudflare_api_token_permission_groups_list" "all" {}
+
+locals {
+  dns_write = element(
+    data.cloudflare_api_token_permission_groups_list.all.result,
+    index(
+      data.cloudflare_api_token_permission_groups_list.all.result.*.name,
+      "DNS Write"
+    )
+  )
+  zone_read = element(
+    data.cloudflare_api_token_permission_groups_list.all.result,
+    index(
+      data.cloudflare_api_token_permission_groups_list.all.result.*.name,
+      "Zone Read"
+    )
+  )
+}
 
 resource "cloudflare_api_token" "cert_manager" {
   name = "cert_manager_${var.environment}"
-  policy {
+  policies = [{
+    effect = "allow"
     permission_groups = [
-      data.cloudflare_api_token_permission_groups.all.zone["DNS Write"],
-      data.cloudflare_api_token_permission_groups.all.zone["Zone Read"]
+      { id = local.dns_write.id },
+      { id = local.zone_read.id }
     ]
     resources = {
       "com.cloudflare.api.account.zone.*" = "*"
     }
-  }
+  }]
 }
 
 resource "bitwarden_secret" "cert-manager" {
