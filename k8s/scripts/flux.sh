f245524bb4d82a81d52640828327d64e458f1b93
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-09-30 16:02:45 -0600

    fix flux script
    Stupid variable parsing

--- k8s/scripts/flux.sh
+++ k8s/scripts/flux.sh
@@ -9,12 +9,17 @@ cleanup() {
 }
 trap cleanup EXIT
 
-helm install flux-operator oci://ghcr.io/controlplaneio-fluxcd/charts/flux-operator \
-  --namespace flux-system \
-  --create-namespace
+# helm install flux-operator oci://ghcr.io/controlplaneio-fluxcd/charts/flux-operator \
+#   --namespace flux-system \
+#   --create-namespace
 
 bws run -- "echo \"\$GITHUB_APP_KEY\" > $KEY_FILE"
-bws run -- "flux create secret githubapp flux-system --app-id=$GITHUB_APP_ID --app-installation-id=$GITHUB_INSTALLATION_ID  --app-private-key=$KEY_FILE"
 
-kubectl apply -f ./scripts/flux-dev-instance.yaml
+
+bws run -- "flux create secret githubapp flux-system \
+  --app-id=\"\$GITHUB_APP_ID\" \
+  --app-installation-id=\"\$GITHUB_INSTALLATION_ID\" \
+  --app-private-key=\"$KEY_FILE\""
+
+# kubectl apply -f ./scripts/flux-dev-instance.yaml
 
