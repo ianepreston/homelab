6cda655fcf07721c30dc59dff1081aca3cf2e317
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-11-03 08:39:38 -0700

    flatten flake
    Going to try and be able to do everything from root
    Hopping around based on activity is inconvenient and sketchy

--- flake.nix
+++ flake.nix
@@ -1,10 +1,17 @@
 {
   description = "Just to run renovate";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    talhelper.url = "github:budimanjojo/talhelper";
+  };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      talhelper,
+    }:
     let
       supportedSystems = [
         "x86_64-linux"
@@ -12,21 +19,83 @@
         "x86_64-darwin"
         "aarch64-darwin"
       ];
+      talosctlVersions = {
+
+        "1.11.3" = {
+          version = "1.11.3";
+          sha256 = "sha256-fNOg0lqPLeo0PCgVQxzAfT6AurEk58PiJziBncCcGz8=";
+        };
+      };
+      # nix-prefetch-url https://github.com/siderolabs/talos/releases/download/v1.11.1/talosctl-linux-amd64
+      # "1.11.1" = {
+      #   version = "1.11.1";
+      #   sha256 = "sha256-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=";
+      # };
+      selectedTalosctlVersion = "1.11.3"; # Change this to switch versions
+      talosctlBinary =
+        {
+          pkgs,
+          system,
+          versionInfo,
+        }:
+        pkgs.stdenv.mkDerivation {
+          pname = "talosctl";
+          version = versionInfo.version;
+          src = pkgs.fetchurl {
+            url = "https://github.com/siderolabs/talos/releases/download/v${versionInfo.version}/talosctl-${
+              if pkgs.stdenv.isDarwin then "darwin" else "linux"
+            }-amd64";
+            sha256 = versionInfo.sha256;
+          };
+          dontUnpack = true;
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src $out/bin/talosctl
+            chmod +x $out/bin/talosctl
+          '';
+        };
+
       forEachSupportedSystem =
         f:
         nixpkgs.lib.genAttrs supportedSystems (
           system:
-          f {
-            pkgs = import nixpkgs { inherit system; };
-          }
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+            versionInfo = talosctlVersions.${selectedTalosctlVersion};
+            talosctlPkg = talosctlBinary { inherit pkgs system versionInfo; };
+            talhelperPkg = talhelper.packages.${system}.default;
+          in
+          f { inherit pkgs talosctlPkg talhelperPkg; }
         );
     in
     {
       devShells = forEachSupportedSystem (
-        { pkgs }:
+        {
+          pkgs,
+          talhelperPkg,
+          talosctlPkg,
+        }:
         {
           default = pkgs.mkShell {
-            packages = with pkgs; [ renovate ];
+            packages = with pkgs; [
+              renovate
+              terraform
+              bws
+              talosctlPkg
+              talhelperPkg
+              kubectl
+              kubernetes-helm
+              kustomize
+              clusterctl
+              cilium-cli
+              fluxcd
+              bws
+              bitwarden-cli
+              go-task
+            ];
           };
         }
       );
